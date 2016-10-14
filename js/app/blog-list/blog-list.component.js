9e8dee9c6cd5d3ac06b89458cd3cc6d66d89757a
uiandwe <uiandwe@gmail.com>
2016-10-14 19:48:53 +0900

    Looping in a Template with ng repeat

--- js/app/blog-list/blog-list.component.js
+++ js/app/blog-list/blog-list.component.js
@@ -5,7 +5,16 @@ angular.module('blogList', []).
         //template: "<div  ><h1 class='new-class'>{{title}}</h1><button ng-click='someClickTest()'>click me</button></div>",
         templateUrl: '/template/blog-list.html',
         controller: function($scope){
-            console.log("!");
+
+            var blogItems = [
+                {title: "soem Title", id:1, description: "this si a book"},
+                {title: " Title", id:2, description: "thiasdfsadfook"},
+                {title: "adf Title", id:3, description: "asdf sad fsthis si a book"},
+                {title: "123  asdf Title", id:4, description: "12 312 31 2this si a book"},
+            ]
+
+            $scope.items = blogItems;
+
             $scope.title = "hi there"
             $scope.clicks = 0
             $scope.someClickTest = function(){
