b2cdee648a0c6b023a8ee48b828efa96a23bbb6c
uiandwe <uiandwe@gmail.com>
2016-10-17 20:51:40 +0900

    ngResource for mapping to a Restful

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,44 +3,23 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($http, $location, $routeParams, $scope){
+        controller: function(Post, $http, $location, $routeParams, $scope){
 
-
-
-            $http.get("/json/posts.json").then(successCallback, errorCallback);
-
-            function successCallback(response, status, config, statusText){
+            Post.query(function(data){
                 $scope.notFound = true;
-
-                var blogItems = response.data;
-                $scope.posts = blogItems;
-
-                angular.forEach(blogItems, function(blog){
+                angular.forEach(data, function(blog){
 
                     if (blog.id == $routeParams.id){
                         $scope.notFound = false;
                         $scope.post = blog;
                     }
                 })
-
-            }
-
-            function errorCallback(response, status, config, statusText){
-
-            }
+            });
 
             if ($scope.notFound){
                 console.log("Not found");
                 $location.path("/404")
             }
-
-
-            console.log($routeParams);
-
-
-
-
-
 
     }
 });
--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -5,18 +5,12 @@ angular.module('blogList', []).
     component('blogList', {
         //template: "<div  ><h1 class='new-class'>{{title}}</h1><button ng-click='someClickTest()'>click me</button></div>",
         templateUrl: '/templates/blog-list.html',
-        controller: function($routeParams, $scope){
+        controller: function(Post, $routeParams, $scope){
 
-            console.log($routeParams.id);
 
-            var blogItems = [
-                {title: "soem Title", id:1, description: "this si a book"},
-                {title: " Title", id:2, description: "thiasdfsadfook"},
-                {title: "adf Title", id:3, description: "asdf sad fsthis si a book"},
-                {title: "123  asdf Title", id:4, description: "12 312 31 2this si a book"},
-            ]
 
-            $scope.items = blogItems;
+            $scope.items = Post.query();
+
 
             $scope.title = "hi there"
             $scope.clicks = 0
