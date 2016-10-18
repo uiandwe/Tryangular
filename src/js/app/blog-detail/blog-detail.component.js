eefb6c18a9107b93fa31f27d690d9fc2bf0029df
uiandwe <uiandwe@gmail.com>
2016-10-18 22:09:21 +0900

    Confirm Comment Delete

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -17,6 +17,10 @@ angular.module('blogDetail').
                 })
             });
 
+            $scope.deleteComment = function(comment){
+                $scope.post.comments.splice(comment, 1)
+            }
+
             $scope.addReply = function(){
                 console.log($scope.reply)
                 $scope.post.comments.push($scope.reply);
