ad0d592a124c18e17ae6ba815e72a61e6f933364
uiandwe <uiandwe@gmail.com>
2016-10-19 19:23:32 +0900

    Angular Columns

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -10,8 +10,25 @@ angular.module('blogList', []).
                 $location.path("/blog/"+ post.id)
             }
 
-            $scope.items = Post.query();
+            $scope.items = Post.query(function(data){
+                var numCols = 6;
+                $scope.cssClass = 'col-sm-'+ numCols;
+                $scope.items = data;
+                $scope.colItems = chunkArrayInGroups(data, numCols)
+            }, function(errorData){
 
+            });
+
+            function chunkArrayInGroups(array, unit){
+                var results = [],
+                    length = Math.ceil(array.length / unit);
+                for(var i = 0; i< length; i++){
+                    results.push(array.slice(i * unit, (i + 1) * unit));
+
+                }
+
+                return results;
+            }
             
         }
     });
